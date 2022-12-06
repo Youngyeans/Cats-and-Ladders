d2f3ff17e8a2ce5bf3085c07ed778875de38f500
ัyoungyeans <94213667+Youngyeans@users.noreply.github.com>
2022-12-06 18:17:04 +0700

    Delete PSCP project directory

--- PSCP project/state/game_play.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from state.state import State
-
-class GamePlay(State):
-    def __init__(self):
-        State.__init__(self)
-    
-    def upload(self):
-        pass
--- PSCP project/state/state.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class State:
-    def __init__(self):
-        self.game = 0
-        self.state_current = 0
-
-    def setstate():
-        pass
