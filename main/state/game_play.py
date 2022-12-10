9f56676fb993dc81f1e016f42e834b226fbe3ffb
ัyoungyeans <94213667+Youngyeans@users.noreply.github.com>
2022-12-10 21:15:23 +0700

    Add files via upload

---
+++ main/state/game_play.py
@@ -0,0 +1,9 @@
+import pygame
+from state.state import State
+
+class GamePlay(State):
+    def __init__(self):
+        State.__init__(self)
+    
+    def upload(self):
+        pass
---
+++ main/state/state.py
@@ -0,0 +1,9 @@
+import pygame
+
+class State:
+    def __init__(self):
+        self.game = 0
+        self.state_current = 0
+
+    def setstate():
+        pass
